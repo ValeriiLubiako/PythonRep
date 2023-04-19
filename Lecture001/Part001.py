7d4eb7b59a3be64b84aa7aa8f1dc1603e34dc969
= <idval@imedia.ru>
2023-04-19 23:37:37 +0300

    Lecture 1 Part 1

---
+++ Lecture001/Part001.py
@@ -0,0 +1,26 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+a = 5
+b = 5.89
+c = 'hello'
+print(f"{a} + {b} + {c}")
+print("{} - {} - {}".format(a,b,c))
+c = 5.89
+print(c)
+print(type(c))
+c = int(c)
+print(type(c))
+print(c)
+a = 12.1234567
+b = 0.234
+print(round(a*b,3))
+#
+iter = 2
+iter += 3  # iter = iter + 3
+iter -= 4  # iter = iter - 4
+iter *= 5  # iter = iter * 5
+iter /= 5  # iter = iter / 5
+iter //= 5 # iter = iter // 5
+iter %= 5  # iter = iter % 5
+iter **= 5 # iter = iter ** 5
