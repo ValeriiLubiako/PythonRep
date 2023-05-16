afe357d0d61f2c6db3e32b74a2abd425baa33494
= <idval@imedia.ru>
2023-05-16 23:12:52 +0300

    Preliminary Sem009 commit

---
+++ Sem009/Program002.py
@@ -0,0 +1,9 @@
+#   Задача 42: Узнать какая максимальная households в зоне минимального значения population.
+
+import pandas as pd
+
+a = pd.read_csv('sample_data/california_housing_train.csv')   #  считываем файл
+
+print([['population']==f['population'].min()]f[f['['households'].max())
+
+#  https://colab.research.google.com/drive/1cy4B-3peyRjmzBCYh__1AcTPNx8_r78N?usp=sharing
