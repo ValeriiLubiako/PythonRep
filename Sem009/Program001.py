5d5573c797784e8c4802df9e2572bfc612b06491
= <idval@imedia.ru>
2023-05-17 18:32:36 +0300

    Sem009 final commit

--- Sem009/Program001.py
+++ Sem009/Program001.py
@@ -1,22 +1,19 @@
 # Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data. 
 # Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
-
-
-import pandas as pd
-f = pd.read_csv('sample_data/california_housing_train.csv')
-print(f[(f['population']<500) & (f['population']>0)]['median_house_value'].mean())
-
+#
+#  ссылка на collab:
+#  https://colab.research.google.com/drive/1cy4B-3peyRjmzBCYh__1AcTPNx8_r78N?usp=sharing
+#
 import pandas as pd  #   подгружаем библиотеку pandas
 a = pd.read_csv('sample_data/california_housing_train.csv')   #  считываем файл
 #  выделяем столбец "средняя стоимость жилья", значения отобраны в соответствии с заданными в задаче условиями
 #  на  population, получили 1601 запись
 b = a[(a['population']<500) & (a['population']>0)]['median_house_value']
 # вычисляем среднее значение по столбцу
-b.mean()
+print(b.mean())
 
+# Можно не расписывать промежуточные вычисления и записать в блее компактной форме
 
-
-
-
-#  ссылка на collab:
-#  https://colab.research.google.com/drive/1cy4B-3peyRjmzBCYh__1AcTPNx8_r78N?usp=sharing
+import pandas as pd  #   подгружаем библиотеку pandas
+a = pd.read_csv('sample_data/california_housing_train.csv')   #  считываем файл
+print(a[(a['population']<500) & (a['population']>0)]['median_house_value'].mean())
--- Sem009/Program002.py
+++ Sem009/Program002.py
@@ -1,9 +1,21 @@
 #   Задача 42: Узнать какая максимальная households в зоне минимального значения population.
-
-import pandas as pd
-
-a = pd.read_csv('sample_data/california_housing_train.csv')   #  считываем файл
-
-print([['population']==f['population'].min()]f[f['['households'].max())
-
+#
+#  ссылка на collab:
 #  https://colab.research.google.com/drive/1cy4B-3peyRjmzBCYh__1AcTPNx8_r78N?usp=sharing
+#
+import pandas as pd
+c = pd.read_csv('sample_data/california_housing_train.csv')   #  считываем файл
+
+# отбираем  записи в соответствии с заданным критерием поиска - минимальным значением в столбце 'population'
+# 
+d = c[c['population']==c['population'].min()]
+# среди отобранных записей находим запись (записи) с максимальн значение в колонке 'households'
+print(d['households'].max())
+#
+#
+# Можно не расписывать промежуточные вычисления и записать в блее компактной форме
+#
+import pandas as pd  #   подгружаем библиотеку pandas
+c = pd.read_csv('sample_data/california_housing_train.csv')
+print(c[c['population']==c['population'].min()]['households'].max())
+#
